1356105b861ae5466258e15a47d30eab53bffb68
Samuel-Muriuki <samuel.muriuki@student.moringaschool.com>
2022-03-28 19:58:34 +0300

    Routing and creating the view functions

---
+++ Personal-Gallery/gallery/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+
+# URL Configurations
+urlpatterns = [
+    path('', views.home_gallery, name = 'home_gallery'),
+    path('add_photo/<str:primary_key>', views.adding_photos, name = 'adding_photos'),
+    path('view_photo', views.viewing_photos, name = 'viewing_photos')
+]
--- Personal-Gallery/gallery/views.py
+++ Personal-Gallery/gallery/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home_gallery(request):
+    return render(request, 'gallery/home_gallery.html')
+
+def adding_photos(request, primary_key):
+    return render(request, 'gallery/adding_photos.html')
+
+def viewing_photos(request):
+    return render(request, 'gallery/viewing_photos.html')
