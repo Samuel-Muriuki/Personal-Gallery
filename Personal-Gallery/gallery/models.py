7cb0a9e80203878fe598870251c834e24bdb6aab
Samuel-Muriuki <samuel.muriuki@student.moringaschool.com>
2022-03-29 06:31:59 +0300

    Creating the migrations

--- Personal-Gallery/gallery/models.py
+++ Personal-Gallery/gallery/models.py
@@ -1,4 +1,3 @@
-from tkinter import CASCADE
 from unicodedata import category
 from django.db import models
 
@@ -18,7 +17,10 @@ class Location(models.Model):
 
 
 class Image(models.Model):
-    category = models.ForeignKey(Category, on_delete=CASCADE)
-    location = models.ForeignKey(Location, on_delete=CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
     description = models.CharField(max_length=300, blank=False)
     image = models.CharField(max_length=500, null=False, blank=False)
+
+    def __str__(self):
+        return self.description
