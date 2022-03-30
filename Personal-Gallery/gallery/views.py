14aa817c90035194ed7bae85542aa0e653c9e280
Samuel-Muriuki <samuel.muriuki@student.moringaschool.com>
2022-03-30 07:32:00 +0300

    Displaying images in the gallery

--- Personal-Gallery/gallery/views.py
+++ Personal-Gallery/gallery/views.py
@@ -1,3 +1,4 @@
+from email.mime import image
 from multiprocessing import context
 from unicodedata import category
 from django.shortcuts import render
@@ -6,11 +7,13 @@ from .models import Category, Image
 # Create your views here.
 def home_gallery(request):
     categories = Category.objects.all()
-    context = {'categories': categories}
+    images = Image.objects.all()
+    context = {'categories': categories, 'images': images}
     return render(request, 'home.html', context)
 
 def adding_photos(request):
     return render(request, 'gallery/adding_photos.html')
 
 def viewing_photos(request, pk):
+    # photo = Image.objects.get(id=pk)
     return render(request, 'gallery/viewing_photos.html')
