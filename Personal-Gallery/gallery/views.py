bcb309b86133761b633bc0b5ee2f17628bc05487
Samuel-Muriuki <samuel.muriuki@student.moringaschool.com>
2022-03-30 06:18:13 +0300

    Showing the category output

--- Personal-Gallery/gallery/views.py
+++ Personal-Gallery/gallery/views.py
@@ -1,8 +1,13 @@
+from multiprocessing import context
+from unicodedata import category
 from django.shortcuts import render
+from .models import Category, Image
 
 # Create your views here.
 def home_gallery(request):
-    return render(request, 'home.html')
+    categories = Category.objects.all()
+    context = {'categories': categories}
+    return render(request, 'home.html', context)
 
 def adding_photos(request):
     return render(request, 'gallery/adding_photos.html')
