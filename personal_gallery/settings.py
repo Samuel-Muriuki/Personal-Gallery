069b4ce0c89cf18599ce21913523866cd0e14e1a
Samuel-Muriuki <samuel.muriuki@student.moringaschool.com>
2022-03-30 22:57:26 +0300

    Deployment010

--- personal_gallery/settings.py
+++ personal_gallery/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = 'django-insecure--h0%8j9@_r46wg%taer$%j(d+eak5$_z+9y1e^4_u3elw(ze'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
