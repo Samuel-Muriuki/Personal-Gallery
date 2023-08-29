43f927e5dc9583d328d0c550426ad48ee6faace5
Samuel-Muriuki <sammkimberly@gmail.com>
2023-08-29 23:34:20 +0300

    Deploy-12 to vercel

--- build.sh
+++ build.sh
@@ -3,14 +3,6 @@
 # Build the project
 echo "Building the project..."
 sudo apt-get install libpq-dev
-pip install psycopg2-binarypip install psycopg2pip install wheel setuptools pip --upgrade
-pip3 install wheel
-
-python -m pip install wheel setuptools pip --upgrade
-python3 -m pip install wheel setuptools pip --upgrade
-py -m pip install wheel setuptools pip --upgrade
-pip install wheel setuptools pip --upgrade
-pip3 install wheel setuptools pip --upgrade
 
 python3 -m pip install -r requirements.txt
 
